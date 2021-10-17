c44d947f1fde40eceb06d32949f6e24d657b889b
unknown <golden.star0228@gmail.com>
2021-10-17 16:22:24 -0400

    updated web3-api

--- src/utils/web3_api.ts
+++ src/utils/web3_api.ts
@@ -12,9 +12,8 @@ export const ethConnect = async () => {
       await wnd.ethereum.request({
         method: 'wallet_switchEthereumChain',
         params: [{ chainId: '0x2A' }],
-      });
-    } catch (switchError: any) {
-    }
+      })
+    } catch (switchError: any) {}
 
     await wnd.ethereum.send('eth_requestAccounts')
     web3 = new Web3(Web3.givenProvider)
@@ -28,62 +27,189 @@ export const connectToContract = () => {
   contract = new web3.eth.Contract(StarNft.abi, contractConfig.contractAddress)
 }
 
-export const getEthBalance = (addr: string) => new Promise((resolve: (val: number) => void, reject: any) => {
-  web3.eth.getBalance(addr).then((_balance: any) => {
-    const balance = web3.utils.fromWei(_balance, 'ether')
-    resolve(balance)
-  }, (err: any) => {})
-})
+export const getEthBalance = (addr: string) =>
+  new Promise((resolve: (val: number) => void, reject: any) => {
+    web3.eth.getBalance(addr).then(
+      (_balance: any) => {
+        const balance = web3.utils.fromWei(_balance, 'ether')
+        resolve(balance)
+      },
+      (err: any) => {}
+    )
+  })
 
-export const getPrice = () => new Promise((resolve: (val: number) => void, reject: any) => {
-  if (!contract) {
-    reject("Contract is not defined")
-  }
-  contract.methods.PRICE().call().then((res: any) => {
-    resolve(web3.utils.fromWei(res, 'ether'))
-  }, (err: any) => {})
-})
+export const getPrice = () =>
+  new Promise((resolve: (val: number) => void, reject: any) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
 
-export const getMintedTokenCount = () => new Promise((resolve: (val: number) => void, reject: any) => {
-  if (!contract) {
-    reject("Contract is not defined")
-  }
-  contract.methods.mintedTokenCount().call().then((res: any) => {
-    resolve(res)
-  }, (err: any) => {})
-})
+    contract.methods
+      .PRICE()
+      .call()
+      .then(
+        (res: any) => {
+          resolve(web3.utils.fromWei(res, 'ether'))
+        },
+        (err: any) => {}
+      )
+  })
 
-export const getRemainTokenCount = () => new Promise((resolve: (val: number) => void, reject: any) => {
-  if (!contract) {
-    reject("Contract is not defined")
-  }
-  contract.methods.remainTokenCount().call().then((res: any) => {
-    resolve(res)
-  }, (err: any) => {})
-})
+export const getMintedTokenCount = () =>
+  new Promise((resolve: (val: number) => void, reject: any) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
+    contract.methods
+      .mintedTokenCount()
+      .call()
+      .then(
+        (res: any) => {
+          resolve(res)
+        },
+        (err: any) => {}
+      )
+  })
 
-export const setMint = () => new Promise((resolve: (val: number) => void, reject: any) => {
-  if (!contract) {
-    reject("Contract is not defined")
-  }
-  contract.methods.remainTokenCount().call().then((res: any) => {
-    resolve(res)
-  }, (err: any) => {})
-})
+export const getRemainTokenCount = () =>
+  new Promise((resolve: (val: number) => void, reject: any) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
+    contract.methods
+      .remainTokenCount()
+      .call()
+      .then(
+        (res: any) => {
+          resolve(res)
+        },
+        (err: any) => {}
+      )
+  })
 
+export const setMint = () =>
+  new Promise((resolve: (val: number) => void, reject: any) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
+    contract.methods
+      .remainTokenCount()
+      .call()
+      .then(
+        (res: any) => {
+          resolve(res)
+        },
+        (err: any) => {}
+      )
+  })
 
-export function mintNFT(addr: string, mintPricePerToken: number, amount: number = 1) {
-  const priceWei = web3.utils.toWei(mintPricePerToken * amount + "", "ether"); // Convert to wei value
-  const tx = {
-    'from': addr,
-    'to': contractConfig.contractAddress,
-    'gas': 500000,
-    "value": priceWei,
-    'maxPriorityFeePerGas': 1999999987,
-    'data': contract.methods.requestRandomNFT(addr, amount).encodeABI()
-  };
+export const mintNFT = (
+  addr: string,
+  mintPricePerToken: number,
+  amount: number = 1
+) =>
+  new Promise((resolve, reject) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
 
-  web3.eth.sendTransaction(tx).then((e: any) => {
-    console.log(e);
-  });
-}
+    const priceWei = web3.utils.toWei(mintPricePerToken * amount + '', 'ether') // Convert to wei value
+    const tx = {
+      from: addr,
+      to: contractConfig.contractAddress,
+      gas: 500000,
+      value: priceWei,
+      maxPriorityFeePerGas: 1999999987,
+      data: contract.methods.requestRandomNFT(addr, amount).encodeABI(),
+    }
+
+    web3.eth.sendTransaction(tx).then((e: any) => {
+      resolve(e)
+    })
+  })
+
+export const withdrawEth = (address: string) =>
+  new Promise((resolve, reject) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
+
+    const tx = {
+      from: address,
+      to: contractConfig.contractAddress,
+      data: contract.methods.withdrawAll().encodeABI(),
+    }
+    web3.eth.sendTransaction(tx).then((e: any) => {
+      console.log(e)
+      resolve(e)
+    })
+    web3.eth.accounts.signTransaction(
+      tx,
+      process.env.REACT_APP_ACCOUNT_PRIVATE_KEY
+    )
+  })
+
+export const setPause = (address: string, value: boolean) =>
+  new Promise((resolve, reject) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
+
+    const tx = {
+      from: address,
+      to: contractConfig.contractAddress,
+      data: contract.methods.setPause(value + '').encodeABI(),
+    }
+    web3.eth.sendTransaction(tx).then((e: any) => {
+      console.log(e)
+      resolve(e)
+    })
+    web3.eth.accounts.signTransaction(
+      tx,
+      process.env.REACT_APP_ACCOUNT_PRIVATE_KEY
+    )
+  })
+
+export const addWhiteList = (address: string, _address: string) =>
+  new Promise((resolve, reject) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
+
+    const tx = {
+      from: address,
+      to: contractConfig.contractAddress,
+      data: contract.methods.addWhiteList(_address).encodeABI(),
+    }
+    web3.eth.sendTransaction(tx).then((e: any) => {
+      console.log(e)
+      resolve(e)
+    })
+    web3.eth.accounts.signTransaction(
+      tx,
+      process.env.REACT_APP_ACCOUNT_PRIVATE_KEY
+    )
+  })
+
+export const removeWhiteList = (address: string, _address: string) =>
+  new Promise((resolve, reject) => {
+    if (!contract) {
+      reject('Contract is not defined')
+    }
+    
+    const tx = {
+      from: address,
+      to: contractConfig.contractAddress,
+      data: contract.methods.removeWhiteList(_address).encodeABI(),
+    }
+
+    web3.eth.sendTransaction(tx).then((e: any) => {
+      resolve(e)
+      console.log(e)
+    })
+
+    web3.eth.accounts.signTransaction(
+      tx,
+      process.env.REACT_APP_ACCOUNT_PRIVATE_KEY
+    )
+  })
